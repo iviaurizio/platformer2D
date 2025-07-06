929dcc68cabd298966a841fe6beeb3af8a730407
iviaurizio <96687343+iviaurizio@users.noreply.github.com>
2025-07-06 17:17:30 +0200

    Aggiunto bottone setting, e la logica per far cambiare schermata quando lo premi

--- script/gestisci_bottoni.gd
+++ script/gestisci_bottoni.gd
@@ -1,5 +1,15 @@
 extends Control
 
 
-func _on_button_button_down() -> void:
+func _on_play_button_down() -> void:
 	get_tree().change_scene_to_file("res://scene/livelli/gioco.tscn")
+
+
+func _on_setting_button_down() -> void:
+	$Buttons.visible=false
+	$Settings.visible=true
+
+
+func _on_back_to_title_screen_button_down() -> void:
+	$Buttons.visible=true
+	$Settings.visible=false
